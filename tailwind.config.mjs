ccbb4cab3402a3ff9157d52f1a9e65303c409fca
Carlos <karlosky12@gmail.com>
2024-08-06 14:54:00 +0200

    Header proyectado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,21 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
-			/*Básicos*/
-			'white': '#ffffff',
+			/*Normales*/
+			'white': '#FFFFFF',
 			'black': '#000000',
-			/*Básicos*/
-			/*Interface*/
-			'interface-black': '#16161e;',
-			/*Interface*/
 			/*Corporativos*/
-			'optimafar-azul': '#1F9DA5',
-			'optimafar-gris': '#777676',
-			/*Corporativos*/
+			'clstudio-pink': '#CC147F',
 		},
 		extend: {},
 	},
 	plugins: [
-		animations
 	],
 }
