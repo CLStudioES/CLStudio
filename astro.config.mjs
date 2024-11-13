f08b503f6ee3f9b6fc45b8d420cc933e607d2335
Carlos López Merino <karlosky12@gmail.com>
2024-11-13 15:17:21 +0100

    Modificacion de Contacto y funcionalidad completada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
@@ -7,9 +7,15 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  routes: [{
-    path: '/index',
-    handler: () => import('./pages/index.astro')
-  }],
-  adapter: vercel()
+  routes: [
+    {
+      path: "/index",
+      handler: () => import("./pages/index.astro"),
+    },
+    {
+      path: "/result",
+      handler: () => import("./pages/result.astro"),
+    },
+  ],
+  adapter: vercel(),
 });
